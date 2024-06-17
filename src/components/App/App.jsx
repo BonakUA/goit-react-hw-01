5416284c3b6964191b97d18be959a86c09c7ef1d
BonakUA <bonak79@gmail.com>
2024-06-17 03:34:17 +0300

    third task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,15 @@ import Profile from "../Profile/Profile.jsx";
 import userData from "../../userData.json";
 import FriendList from "../FriendList/FriendList.jsx";
 import friends from "../../friends.json"
+import TransactionHistory from "../TransactionHistory/TransactionHistory"
+import transactions from "../../TransactionHistory.json"
 
 export default function App() {
     return (
         <div className={css.container}>
             <Profile userProfile={userData} />
             <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
         </div>
     );
 }
